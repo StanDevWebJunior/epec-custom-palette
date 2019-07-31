f43998f57c322eb0b972c806ca1e2055adf07d1e
yann <yannelogegrah@gmail.com>
2019-07-31 15:50:11 +0200

    Step 2

--- epec-custom-palette.php
+++ epec-custom-palette.php
@@ -5,3 +5,37 @@ Description: un plugin pour personnaliser une palette de couleur
 Version: 0.1
 Author: Too Luck
 */
+
+function mytheme_setup_theme_supported_features() {
+    add_theme_support( 'editor-color-palette', array(
+        array(
+            'name' => __( 'strong magenta', 'themeLangDomain' ),
+            'slug' => 'strong-magenta',
+            'color' => '#6128A1',
+        ),
+        array(
+            'name' => __( 'light grayish magenta', 'themeLangDomain' ),
+            'slug' => 'light-grayish-magenta',
+            'color' => '#E94B59',
+        ),
+        array(
+            'name' => __( 'Very Dark Gray ', 'themeLangDomain' ),
+            'slug' => 'Very Dark Gray',
+            'color' => '#111111',
+        ),
+        array(
+            'name' => __( 'White', 'themeLangDomain' ),
+            'slug' => 'White',
+            'color' => '#FFFFFF',
+        ),
+        array(
+            'name' => __( 'Red', 'themeLangDomain' ),
+            'slug' => 'Red',
+            'color' => '#FF0000',
+        ),
+    ) );
+}
+ 
+add_action( 'after_setup_theme', 'mytheme_setup_theme_supported_features' );
+
+
