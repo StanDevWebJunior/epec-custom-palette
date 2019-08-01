44e3357a41647e69cc78a887b185d1746ab81e17
yann <yannelogegrah@gmail.com>
2019-08-01 12:42:01 +0200

    Plugin epec-custom-palette

--- epec-custom-palette.php
+++ epec-custom-palette.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Custom Colors Palette by Promo EPEC
+Plugin Name: epec-custom-palette
 Description: un plugin pour personnaliser une palette de couleur
 Version: 0.1
 Author: Too Luck
