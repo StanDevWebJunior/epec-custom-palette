75d413ea16b3e2e0efbdebaed8ae33e204fc8f7d
yann <yannelogegrah@gmail.com>
2019-08-01 12:17:59 +0200

    Plugin epec-custom-palette

--- epec-custom-palette.php
+++ epec-custom-palette.php
@@ -9,29 +9,24 @@ Author: Too Luck
 function mytheme_setup_theme_supported_features() {
     add_theme_support( 'editor-color-palette', array(
         array(
-            'name' => __( 'strong magenta', 'themeLangDomain' ),
-            'slug' => 'strong-magenta',
+            'name' => __( 'tomato', 'epec-custom-palette'),
+            'slug' => 'tomato',
+            'color' => '#FF6347',
+        ),
+        array(
+            'name' => __( 'purple', 'epec-custom-palette'),
+            'slug' => 'purple',
             'color' => '#6128A1',
         ),
         array(
-            'name' => __( 'light grayish magenta', 'themeLangDomain' ),
-            'slug' => 'light-grayish-magenta',
-            'color' => '#E94B59',
+            'name' => __( 'green', 'epec-custom-palette'),
+            'slug' => 'green',
+            'color' => '#008000',
         ),
         array(
-            'name' => __( 'Very Dark Gray ', 'themeLangDomain' ),
-            'slug' => 'Very Dark Gray',
-            'color' => '#111111',
-        ),
-        array(
-            'name' => __( 'White', 'themeLangDomain' ),
-            'slug' => 'White',
-            'color' => '#FFFFFF',
-        ),
-        array(
-            'name' => __( 'Red', 'themeLangDomain' ),
-            'slug' => 'Red',
-            'color' => '#FF0000',
+            'name' => __( 'orange', 'epec-custom-palette'),
+            'slug' => 'orange',
+            'color' => '#FF4500',
         ),
     ) );
 }
@@ -39,3 +34,10 @@ function mytheme_setup_theme_supported_features() {
 add_action( 'after_setup_theme', 'mytheme_setup_theme_supported_features' );
 
 
+function my_scripts() {
+    wp_enqueue_script( 'jquery' );
+    wp_register_style( 'prefix-style', plugins_url('app.css', __FILE__) );
+    wp_enqueue_style( 'prefix-style' );
+    }
+    add_action('wp_enqueue_scripts','my_scripts');
+    ?>
